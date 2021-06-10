05c2b03a9ed0e04bb200169dfa5f6979a32e952e
akshitone <akshitone8@gmail.com>
2021-06-10 17:17:26 +0530

    handle cors error

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/feed", feedRoutes);
